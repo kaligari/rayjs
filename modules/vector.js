e7bb6cf7578640cea41abe91f525189076868f7a
Sebastian Kopeć <skopec@x-one.com.pl>
2020-11-22 12:08:48 +0100

    Vectors

--- modules/vector.js
+++ modules/vector.js
@@ -1,11 +1,35 @@
 export default class Vector {
-    constructor(x, y, angle, length, power) {
+    constructor(x, y, z) {
         this.x = x
         this.y = y
-        this.angle = angle
-        this.length = length
-        this.power = power
-        this.collision = null
-        this.collisionDistance = null
+        this.z = z
+    }
+
+    dotProduct(vector) {
+        return this.x * vector.x + this.y * vector.y + this.z * vector.z
+    }
+
+    get magnitude() {
+        return Math.sqrt(this.dotProduct(this))
+    }
+
+    get normalize() {
+        return this.divide(this.magnitude)
+    }
+
+    add(vector) {
+        return new Vector(this.x + vector.x, this.y + vector.y, this.z + vector.z)
+    }
+
+    subtract(vector) {
+        return new Vector(this.x - vector.x, this.y - vector.y, this.z - vector.z)
+    }
+
+    multiply(value) {
+        return new Vector(this.x * value, this.y * value, this.z * value)
+    }
+
+    divide(value) {
+        return new Vector(this.x / value, this.y / value, this.z / value)
     }
 }
--- scripts.js
+++ scripts.js
@@ -3,6 +3,11 @@ import Canvas from './modules/canvas.js'
 
 const canvas = new Canvas()
 
+const vect = new Vector(1, -2, -2)
+const vect2 = new Vector(3, 6, 9)
+
+console.log(vect.normalize)
+
 const render = () => {
     // window.requestAnimationFrame(render)
 }
